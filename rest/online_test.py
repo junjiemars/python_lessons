d3441b16bb8f6fa20156f412f3fa0ef63f215b35
南山竹 <junjiemars@gmail.com>
2017-03-22 17:34:07 +0800

    get/post json

--- rest/online_test.py
+++ rest/online_test.py
@@ -7,11 +7,18 @@ import requests
 
 def main():
     """abc"""
-    rep = requests.get('https://httpbin.org/get')
-    if rep.status_code == 200:
-        for item in rep.json():
-            print(item)
+    get = requests.get('https://httpbin.org/get')
+    if get.status_code == 200:
+        for i in get.json():
+            print(i)
+    print(get.headers)
+
+    jdata = {"xyz": {"abc": 0x11223344}}
+    post = requests.post(url='https://httpbin.org/post',
+                         json=jdata,
+                         headers={"Content-Type": 'application/json'})
+    print(post.headers)
+
 
 if __name__ == '__main__':
     sys.exit(main())
-
